b8caf015f4d2cffb114fbf762df8e41d7a0c7beb
phucv <phucv@DESKTOP-GI5FGLR>
2017-05-24 23:02:12 +0700

    Add project files.

---
+++ PMQL Luong/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMQL_Luong.Entities
+{
+    public class User
+    {
+        public String Id { get; set; }
+        public String Name { get; set; }
+        public String PassWord { get; set; }
+        public String Permission { get; set; }
+    }
+}
