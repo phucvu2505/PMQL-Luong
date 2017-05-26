9be5a41bfee1d2050852a98e25b5505f627ec107
phucv <phucv@DESKTOP-GI5FGLR>
2017-05-26 15:45:54 +0700

    update: mức lương cơ bản

--- PMQL Luong/Properties/Resources.Designer.cs
+++ PMQL Luong/Properties/Resources.Designer.cs
@@ -213,6 +213,16 @@ namespace PMQL_Luong.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Money_Increase {
+            get {
+                object obj = ResourceManager.GetObject("Money_Increase", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap User_Group_Man_Woman_48px {
             get {
                 object obj = ResourceManager.GetObject("User_Group_Man_Woman_48px", resourceCulture);
